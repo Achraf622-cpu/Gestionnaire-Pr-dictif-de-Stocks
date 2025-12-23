42c1632911321bb70a40aa1014a3e56db5ec3bf6
Achraf Hanzaz <hanzazachraf581@gmail.com>
2025-12-23 12:14:59 +0100

    Initialisation du projet

---
+++ src/main/java/com/team/sys_ai/SysAiDePrevisionEtGestionSecApplication.java
@@ -0,0 +1,13 @@
+package com.team.sys_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SysAiDePrevisionEtGestionSecApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SysAiDePrevisionEtGestionSecApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/team/sys_ai/SysAiDePrevisionEtGestionSecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.sys_ai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysAiDePrevisionEtGestionSecApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

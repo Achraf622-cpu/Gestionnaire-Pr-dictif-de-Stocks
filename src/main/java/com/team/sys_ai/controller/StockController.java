220f16035cf55f8f18a6d328ccfb38535fc6296a
Achraf622-cpu <hanzazachraf581@gmail.com>
2025-12-30 10:29:24 +0100

    GPDS-33: #en-cours Implementation du Stock Controller : get stocks for warehouse

---
+++ src/main/java/com/team/sys_ai/controller/StockController.java
@@ -0,0 +1,32 @@
+package com.team.sys_ai.controller;
+
+import com.team.sys_ai.dto.StockDTO;
+import com.team.sys_ai.security.CustomUserDetailsService.CustomUserDetails;
+import com.team.sys_ai.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/api/stocks")
+@RequiredArgsConstructor
+public class StockController {
+
+    private final StockService stockService;
+
+    /**
+     * Get all stocks for a warehouse.
+     */
+    @GetMapping("/entrepot/{entrepotId}")
+    public ResponseEntity<List<StockDTO>> getStocksByEntrepot(
+            @PathVariable Long entrepotId,
+            @AuthenticationPrincipal CustomUserDetails userDetails) {
+        return ResponseEntity.ok(stockService.getStocksByEntrepot(entrepotId, userDetails.getUser()));
+    }
+
+}
